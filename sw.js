102fc951812ce38df61f3048d306921b1f70227f
Aly <alyzopairaly@gmail.com>
2026-08-08 10:29:42 +0300

    InWork Beta: add minimal service worker

---
+++ sw.js
@@ -0,0 +1,1 @@
+self.addEventListener('install',e=>self.skipWaiting());self.addEventListener('activate',e=>self.clients.claim());self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))));
